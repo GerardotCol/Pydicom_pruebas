2f5c875f3bc77c042baf17169e38c00e9036d84c
GerardotCol <ge.dardo@gmail.com>
2025-01-16 13:55:40 -0600

    Se agregan los cambios a los demas archivos

--- my_scp.py
+++ my_scp.py
@@ -1,14 +1,41 @@
-from pynetdicom import AE, debug_logger
+# Se debe de correr en la terminal el 
+# python -m pynetdicom storescu 127.0.0.1 11112 I9900000 -v -cx
+# como SCU
+
+
+import uuid
+from pathlib import Path
+
+from pynetdicom import (
+    AE, debug_logger, evt, AllStoragePresentationContexts,
+    ALL_TRANSFER_SYNTAXES
+)
 
 debug_logger()
 
+def handle_store(event, storage_dir):
+    """Handle EVT_C_STORE events."""
+    try:
+        os.makedirs(storage_dir, exist_ok=True)
+    except:
+        # Unable to create output dir, return failure status
+        return 0xC001
+
+    path = Path(storage_dir) / f"{uuid.uuid4()}"
+    with path.open('wb') as f:
+        # Write the preamble, prefix, file meta information elements
+        #   and the raw encoded dataset to `f`
+        f.write(event.encoded_dataset())
+
+    return 0x0000
+
+handlers = [(evt.EVT_C_STORE, handle_store, ['out'])]
+
 ae = AE()
-ae.add_requested_context("1.2.840.10008.1.1")
-assoc = ae.associate("127.0.0.1", 11112)
-if assoc.is_established:
-    print("Association established with Echo SCP!")
-    status = assoc.send_c_echo()
-    assoc.release()
-else:
-    # Association rejected, aborted or never connected
-    print("Failed to associate")
+storage_sop_classes = [
+    cx.abstract_syntax for cx in AllStoragePresentationContexts
+]
+for uid in storage_sop_classes:
+    ae.add_supported_context(uid, ALL_TRANSFER_SYNTAXES)
+
+ae.start_server(("127.0.0.1", 11112), block=True, evt_handlers=handlers)
